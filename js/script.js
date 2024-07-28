54603cd46521f71cfc0924525f18addde87720e9
walletkun <walletkun@gmail.com>
2024-07-28 17:12:25 +0800

    Added more animation for interactiveness

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+
+//Text writer
+document.addEventListener("DOMContentLoaded", function() {
+    var options = {
+        strings: ["Software Engineer", "Data Scientist"],
+        typeSpeed: 50,       // Speed of typing
+        backSpeed: 50,       // Speed of backspacing
+        backDelay: 1000,     // Time before backspacing
+        startDelay: 0,     // Time before typing starts
+        loop: true,          // Loop the typing animation
+        loopCount: Infinity,
+        showCursor: true
+    };
+    var typed = new Typed("#typed-text", options);
+});
